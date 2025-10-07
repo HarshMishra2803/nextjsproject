aa66d10408bd5c39bfa87e520b2f4423de1e36d4
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-10-07 17:13:14 +0530

    setup

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+      i am footer 
+    </footer>
+  )
+}
+
+export default footer
---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+       i am a navbar
+    </nav>
+  )
+}
+
+export default navbar 
